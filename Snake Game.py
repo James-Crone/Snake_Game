53c32e008c8d9ca5ca9b27f950c22bf780b21874
James-Crone <78932463+James-Crone@users.noreply.github.com>
2022-09-03 00:07:38 +0100

    added functions
    added snakes food, Q closes game R restarts it, cleaned the code up a little, added chomp message when both food and snake axis collide

--- Snake Game.py
+++ Snake Game.py
@@ -1,78 +1,94 @@
 import pygame
 import time
+import random
+
 pygame.init()
 
-
-# snake
-x_change = 0
-y_change = 0
-Y = 400
-X = 400 
-snake_speed = 30 
-
-# colors
-CYAN = (0,255,255)
-RED = (255,0,0)
-WHITE = (255, 255, 255)
-
-# Screen
 WIDTH = 800
 HEIGHT = 800
-clock = pygame.time.Clock() 
-system_font = pygame.font.SysFont(None, 50)
 
-# Screen
+CYAN = (0,255,255)
+RED = (255,0,0)
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+BLACK = (0,0,0)
+PURP = (138,43,226)
+
+snake_speed = 30 
+
+clock = pygame.time.Clock() 
+
+system_font = pygame.font.SysFont(None, 30)
+
 pygame.display.set_caption("Snake Game :)") 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
-# Displays text on screen
 def Lost_Message(msg, color):
     message = system_font.render(msg, True, color)
     WINDOW.blit(message, (WIDTH/3, HEIGHT/2))
 
 
-# main loop
-Game_Over = False
-while not Game_Over:
-    # keeps window open
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            Game_Over = True
-        # moves the snake
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                y_change = -10
-                x_change = 0
-            elif event.key == pygame.K_a:
-                x_change = -10
-                y_change = 0
-            elif event.key == pygame.K_s:
-                y_change = 10
-                x_change = 0
-            elif event.key == pygame.K_d:
-                x_change = 10    
-                y_change = 0
+def Game():
+    Game_Over = False
+    Game_Close = False
 
-    # determins if the snake is out of bounds
-    if X >= WIDTH or X <= 0 or Y >= HEIGHT or Y <= 0:
-        Game_Over = True
-        Lost_Message("You Lost", RED)
+    x_change = 0
+    y_change = 0
+    Y = 400
+    X = 400
+
+    foodx = round(random.randrange(0, WIDTH - 12) / 10.0) * 10.0
+    foody = round(random.randrange(0, WIDTH - 12) / 10.0) * 10.0
+ 
+    while not Game_Over:
+
+        while Game_Close == True:
+            Lost_Message("Press Q to Quit or R to Restart Game", BLACK)
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        Game_Over = True
+                        Game_Close = False
+                    if event.key == pygame.K_r:
+                        Game()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                Game_Over = True
+            
+            # moves the snake
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    y_change = -10
+                    x_change = 0
+                elif event.key == pygame.K_a:
+                    x_change = -10
+                    y_change = 0
+                elif event.key == pygame.K_s:
+                    y_change = 10
+                    x_change = 0
+                elif event.key == pygame.K_d:
+                    x_change = 10    
+                    y_change = 0
+    
+        if X >= WIDTH or X <= 0 or Y >= HEIGHT or Y <= 0:
+            Game_Close = True
+
+        X += x_change
+        Y += y_change
+        WINDOW.fill(PURP)
+        pygame.draw.rect(WINDOW, RED, [X,Y,12,12])
+        pygame.draw.rect(WINDOW, GREEN, [foodx,foody,12,12])
         pygame.display.update()
-        time.sleep(1)
-        
-    # new x and y axis
-    X += x_change
-    Y += y_change
 
-    WINDOW.fill(CYAN)
-    pygame.draw.rect(WINDOW, RED, [X,Y,10,10])
+        if X == foodx and Y == foody:
+            Lost_Message("Chomp!", RED)
+            pygame.display.update()
+        clock.tick(snake_speed)
+    
+    pygame.quit()
+    quit()
 
-    pygame.display.update()
-
-    clock.tick(snake_speed)
-
-
-pygame.display.update()
-pygame.quit()
-quit()
+Game()
